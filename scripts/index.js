57950692bfe5293ea27f26c72ba440f3086b52a7
SuperAlex2013 <122688147+SuperAlex2013@users.noreply.github.com>
2023-05-11 05:52:38 +0300

    fix after review

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,15 @@ const popupEditProfile = document.querySelector(".popup");
 const popupCloseIcon = popupEditProfile.querySelector(".popup__close");
 const userName = document.querySelector(".profile__name");
 const profileInfo = document.querySelector(".profile__info");
-const nameInput = popupEditProfile.querySelector(".popup__input_name");
-const infoInput = popupEditProfile.querySelector(".popup__input_info");
+const nameInput = popupEditProfile.querySelector(".popup__input_type_name");
+const infoInput = popupEditProfile.querySelector(".popup__input_type_info");
 
 const popupToggle = function () {
-  if (popupEditProfile.classList.contains("popup__open")){
+  if (popupEditProfile.classList.contains("popup_opened")){
     nameInput.value = userName.textContent;
     infoInput.value = profileInfo.textContent;
   }
-  popupEditProfile.classList.toggle("popup__open");
+  popupEditProfile.classList.toggle("popup_opened");
 }
 
 const formSubmitHandler = function (evt) {
